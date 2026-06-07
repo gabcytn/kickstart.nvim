5756f6fed11857c27ebc17b8fe5098a30199e9a8
gabcytn <cayetanogabriel03@gmail.com>
2026-06-07 10:12:06 +0800

    feat: tokyonight colorscheme with transparent bg

---
+++ lua/plugins/tokyonight.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 2000,
+    opts = {
+      transparent = true,
+      style = "moon",
+      styles = {
+        sidebars = "transparent",
+        floats = "transparent",
+      },
+    },
+  },
+}
